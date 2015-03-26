cfe76f6588fc06fcdfafcbedd5712248710f12ff
nmoya <nikolasmoya@gmail.com>
2015-03-26 16:26:35 -0300

    Now checking to avoid redownloading the same video twice

--- app.py
+++ app.py
@@ -2,18 +2,22 @@ import sys
 import os
 import subprocess
 import urllib2
+import re
 
 
-def download(url):
+def download(url, title):
 	return subprocess.call(["youtube-dl", "-x",
 							"--audio-format", "mp3",
 							"--audio-quality", "5",
-							url, "-o", "%(title)s.%(ext)s"])
+							url, "-o", title])
 
 
 def clean_string(string):
 	string = string.replace("Lyrics", "")
 	string = string.replace("HQ", "")
+	string = re.sub(r'\([^)]*\)', '', string)
+	string = re.sub(r'\[[^)]*\]', '', string)
+	string = re.sub(r'\{[^)]*\}', '', string)
 	string = string.replace(" - YouTube", "")
 	string = string.replace("&#39;", "'")
 	string = string.replace("&nbsp", " ")
@@ -40,20 +44,40 @@ def get_video_title(url):
 
 if __name__ == "__main__":
 	command = "youtube-dl -t -x --audio-format mp3 --audio-quality 5 %s"
-	if len(sys.argv) != 2:
-		print "python app.py <text file with links>"
+	if len(sys.argv) != 2 and len(sys.argv) != 3:
+		print "python app.py <text file with links> [--force]"
 		sys.exit(1)
 
+	force = False
+	if len(sys.argv) == 3:
+		force = True
+
+	if "downloaded.txt" not in os.listdir("."):
+		fdown = open("downloaded.txt", "w")
+		fdown.close()
+
+	fdown = open("downloaded.txt", "r")
+	content = fdown.read()
+	downloaded = content.split("\n")
+	fdown.close()
+
 	with open(sys.argv[1]) as arq:
-		musics = arq.read().split("\n")
-		downloaded = [f for f in os.listdir(".") if f.endswith(".mp3")]
-		for music in musics:
-			title = get_video_title(music)
+		urls = arq.read().split("\n")
+		# downloaded = [f for f in os.listdir(".") if f.endswith(".mp3")]
+		for video_url in urls:
+			title = get_video_title(video_url)
 			rtn_code = 1
-			if title not in downloaded:
+			if video_url not in downloaded or force:
 				while rtn_code != 0:
 					print "--------------------------------------------------------------"
 					print "Downloading", title, "..."
 					print "--------------------------------------------------------------"
-					rtn_code = download(music)
-				
+					rtn_code = download(video_url, title)
+				if video_url not in downloaded:
+					downloaded.append(video_url)
+			else:
+				print "Title:", title, "was downloaded previously. Re-execute with --force"
+	
+	fdown = open("downloaded.txt", "w")
+	fdown.write("\n".join(downloaded))
+	fdown.close()
